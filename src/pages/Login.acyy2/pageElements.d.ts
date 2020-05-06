2a688505fb88f0f2a2641f8ab63e76265e5b3367
BSwamHR <0966726@hr.nl>
2020-05-06 15:57:08 +0200

    New branch due to trouble merging with master

---
+++ src/pages/Login.acyy2/pageElements.d.ts
@@ -0,0 +1,23 @@
+type PageElementsMap = {
+  "#page1": $w.Page;
+  "#input1": $w.TextInput;
+  "#input2": $w.TextInput;
+  "#button1": $w.Button;
+  "#button2": $w.Button;
+  "#header1": $w.Header;
+  "#container1": $w.HiddenCollapsedElement;
+  "#footer1": $w.Footer;
+  "#image2": $w.Image;
+  "#horizontalMenu1": $w.Menu;
+  "#verticalMenu1": $w.Menu;
+  "#profile1": $w.IFrame;
+  "#group1": $w.Box;
+  "#text12": $w.Text;
+  "#text14": $w.Text;
+  "#text46": $w.Text;
+  "#socialBar1": $w.HiddenCollapsedElement;
+  "#text15": $w.Text;
+  "#text48": $w.Text;
+  "#image4": $w.Image;
+  "#text71": $w.Text;
+}
