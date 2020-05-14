2a06730fdf11bc946259821eee2851d46dbe1efb
HadiDragneel <hadi.zebeleh@gmail.com>
2020-05-14 18:49:03 +0200

    Improved Registration screens, implemented consistent design on 3/4 pages by us (Vacancy creation has not been done yet). Added some animations. Also put both registration pages into 1 registration menu on the top site button

--- src/pages/Contact.k23ci/pageElements.d.ts
+++ src/pages/Contact.k23ci/pageElements.d.ts
@@ -1,18 +1,25 @@
 type PageElementsMap = {
+  "#page1": $w.Page;
+  "#columnStrip1": $w.ColumnStrip;
+  "#column1": $w.Column;
+  "#text3": $w.Text;
+  "#text72": $w.Text;
+  "#googleMaps1": $w.GoogleMap;
+  "#text73": $w.Text;
   "#header1": $w.Header;
   "#image2": $w.Image;
-  "#horizontalMenu1": $w.Menu;
   "#container1": $w.HiddenCollapsedElement;
   "#verticalMenu1": $w.Menu;
   "#profile1": $w.IFrame;
   "#footer1": $w.Footer;
-  "#group1": $w.Box;
   "#image4": $w.Image;
   "#text71": $w.Text;
-  "#text12": $w.Text;
   "#text14": $w.Text;
-  "#text46": $w.Text;
   "#socialBar1": $w.HiddenCollapsedElement;
   "#text15": $w.Text;
+  "#horizontalMenu1": $w.Menu;
+  "#text12": $w.Text;
+  "#text46": $w.Text;
   "#text48": $w.Text;
+  "#group1": $w.Box;
 }
