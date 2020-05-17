211a7b0ec5e4689f9c19f46da959167ee13cd16e
XanYder <xhuisman@gmail.com>
2020-05-17 19:33:16 +0200

    WIP NLP
    In de student registratie; wanneer je submit klikt worden alle keyphrases uit alle workexperiences geprint in de console

--- src/lightboxes/DeleteVacancyConfirmation.b3gey/pageElements.d.ts
+++ src/lightboxes/DeleteVacancyConfirmation.b3gey/pageElements.d.ts
@@ -1,7 +1,2 @@
 type PageElementsMap = {
-  "#page1": $w.Page;
-  "#deleteVacancLB": $w.HiddenCollapsedElement;
-  "#deleteLBYesButton": $w.Button;
-  "#text72": $w.Text;
-  "#deleteLBNoButton": $w.Button;
 }
