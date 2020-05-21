b34db4058ba0026a0a504189b90eb4e1f45bc869
HadiDragneel <hadi.zebeleh@gmail.com>
2020-05-21 18:27:36 +0200

    Rest of the files

---
+++ src/pages/Student_Registration_P1.ndzyv/pageElements.d.ts
@@ -0,0 +1,34 @@
+type PageElementsMap = {
+  "#page1": $w.Page;
+  "#text72": $w.Text;
+  "#studentAccountInfoDataset": $w.dataset;
+  "#text73": $w.Text;
+  "#firstNameInput": $w.TextInput;
+  "#lastNameInput": $w.TextInput;
+  "#text76": $w.Text;
+  "#addressInput": $w.AddressInput;
+  "#dateofbirthInput": $w.DatePicker;
+  "#phoneInput": $w.TextInput;
+  "#nationalityInput": $w.TextInput;
+  "#emailInput": $w.TextInput;
+  "#passwordInput": $w.TextInput;
+  "#captcha1": $w.Captcha;
+  "#tosCheck": $w.Checkbox;
+  "#submitButton": $w.Button;
+  "#header1": $w.Header;
+  "#image2": $w.Image;
+  "#horizontalMenu1": $w.Menu;
+  "#container1": $w.HiddenCollapsedElement;
+  "#verticalMenu1": $w.Menu;
+  "#profile1": $w.IFrame;
+  "#footer1": $w.Footer;
+  "#group1": $w.Box;
+  "#image4": $w.Image;
+  "#text71": $w.Text;
+  "#text12": $w.Text;
+  "#text14": $w.Text;
+  "#text46": $w.Text;
+  "#socialBar1": $w.HiddenCollapsedElement;
+  "#text15": $w.Text;
+  "#text48": $w.Text;
+}
