3e9f2cf91dfd6b239c8b50b1fa933b8c2e0fe180
Blackreaper5 <amaterasu666111@hotmail.com>
2020-06-04 15:59:39 +0200

    Add tensorflow js USE

--- src/pages/Home.c1dmp/Home.js
+++ src/pages/Home.c1dmp/Home.js
@@ -1,9 +1,12 @@
-import {denseModel} from 'backend/tensorFlowDemo';
+// import {denseModel} from 'backend/tensorFlowDemo';
+import {tokenize} from 'backend/preProcess';
 
 $w.onReady(function () {
-    denseModel().then(model => {
-        console.log("[X] Model: ", model)
-    }, error => {
-        console.log("[X] Error: ", error)
-    })
+    // denseModel().then(model => {
+    //     console.log("[X] Model: ", model)
+    // }, error => {
+    //     console.log("[X] Error: ", error)
+    // })
+
+    tokenize()
 });
