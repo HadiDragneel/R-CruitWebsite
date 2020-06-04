80eda1b327aaff13e18409b3c6e490e9566822e5
Blackreaper5 <amaterasu666111@hotmail.com>
2020-06-04 20:50:17 +0200

    Add calculation of % match

--- src/pages/Home.c1dmp/Home.js
+++ src/pages/Home.c1dmp/Home.js
@@ -1,12 +1,27 @@
-// import {denseModel} from 'backend/tensorFlowDemo';
-import {tokenize} from 'backend/preProcess';
+import {getScore} from 'backend/model';
 
-$w.onReady(function () {
-    // denseModel().then(model => {
-    //     console.log("[X] Model: ", model)
-    // }, error => {
-    //     console.log("[X] Error: ", error)
-    // })
+$w.onReady(async function () {
 
-    tokenize()
+    // TODO: This should be on the overview page for R'Cruit
+    const THRESHOLD = 0.6
+
+    const qualifications = [
+        "Should be good with JavaScript",
+        "You should be fluent in German",
+        "Solid experience with English grammar is preferred"
+    ]
+
+    const experiences = [
+        "I am fluent in English",
+        "I have a strong understanding of the German language",
+        "I have a lot of experience with JavaScript",
+        "I have been a cook at Happy Italy"
+    ]
+    getScore(qualifications, experiences).then(score => {
+        console.log(`The % match is: ${score * 100}%`)
+
+        if (score > THRESHOLD) {
+            // TODO: Write to data collection
+        }
+    })
 });
