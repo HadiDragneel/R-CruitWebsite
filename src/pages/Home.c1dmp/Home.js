52ea768fd6f5dfea38e9157236d0b874739d9e15
Blackreaper5 <amaterasu666111@hotmail.com>
2020-06-05 19:53:50 +0200

    Add Todo's

--- src/pages/Home.c1dmp/Home.js
+++ src/pages/Home.c1dmp/Home.js
@@ -2,9 +2,10 @@ import {getScore} from 'backend/model';
 
 $w.onReady(async function () {
 
-    // TODO: This should be on the overview page for R'Cruit
+    // TODO: This code below block should be on the overview page for R'Cruit
     const THRESHOLD = 0.6
 
+    // TODO: Qualifications and experiences should be read from the data collections
     const qualifications = [
         "Should be good with JavaScript",
         "You should be fluent in German",
@@ -17,11 +18,14 @@ $w.onReady(async function () {
         "I have a lot of experience with JavaScript",
         "I have been a cook at Happy Italy"
     ]
+
     getScore(qualifications, experiences).then(score => {
         console.log(`The % match is: ${score * 100}%`)
 
         if (score > THRESHOLD) {
-            // TODO: Write to data collection
+            // TODO: Write to MatchingData collection
+            //  PS: Don't forget to write the reference of the CV and Vacancy too
         }
     })
+    // TODO: This code block above should be on the overview page for R'Cruit
 });
