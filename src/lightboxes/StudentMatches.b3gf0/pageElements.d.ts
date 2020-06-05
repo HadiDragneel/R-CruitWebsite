143c26ce68a190ebe8b924a08b614e2fee6373bd
TimHoreweg <timhoreweg@hotmail.com>
2020-06-05 13:58:02 +0200

    student matches page design implemented

---
+++ src/lightboxes/StudentMatches.b3gf0/pageElements.d.ts
@@ -0,0 +1,8 @@
+type PageElementsMap = {
+  "#page1": $w.Page;
+  "#lightbox1": $w.HiddenCollapsedElement;
+  "#text74": $w.Text;
+  "#text72": $w.Text;
+  "#button1": $w.Button;
+  "#table1": $w.Table;
+}
