b07b122f7085c57dfbff204fa6fc5195815c42a9
HadiDragneel <hadi.zebeleh@gmail.com>
2020-06-07 15:13:14 +0200

    Matching function in backend with demo

--- src/pages/Home.c1dmp/Home.js
+++ src/pages/Home.c1dmp/Home.js
@@ -3,32 +3,6 @@ import wixData from 'wix-data';
 
 $w.onReady(async function () {
 
-    // TODO: This code below block should be on the overview page for R'Cruit
-    const THRESHOLD = 0.6
-
-    // TODO: Qualifications and experiences should be read from the data collections
-    const qualifications = [
-        "Should be good with JavaScript",
-        "You should be fluent in German",
-        "Solid experience with English grammar is preferred"
-    ]
-
-    const experiences = [
-        "I am fluent in English",
-        "I have a strong understanding of the German language",
-        "I have a lot of experience with JavaScript",
-        "I have been a cook at Happy Italy"
-    ]
-
-    getScore(qualifications, experiences).then(score => {
-        console.log(`The % match is: ${score * 100}%`)
-
-        if (score > THRESHOLD) {
-            // TODO: Write to MatchingData collection
-            //  PS: Don't forget to write the reference of the CV and Vacancy too
-        }
-    })
-    // TODO: This code block above should be on the overview page for R'Cruit
 
 
 });
--- src/pages/Student_Registration_P1.ndzyv/Student_Registration_P1.js
+++ src/pages/Student_Registration_P1.ndzyv/Student_Registration_P1.js
@@ -1,7 +1,7 @@
 import wixData from 'wix-data';
 import wixUsers from 'wix-users';
 import wixLocation from 'wix-location';
-import {getScore} from 'backend/model';
+import {matchStudent} from 'backend/model';
 
 
 $w.onReady(function () {
@@ -23,25 +23,13 @@ $w.onReady(function () {
     //Submit button function
     $w('#submitButton').onClick((event) => {
 
-        const experiences = [
-            "I am a good chef",
-            "I am very funny",
-            "I have a lot of experience with Java"
-        ];
-
-        //email - ab@h.com
-        //_id - 344033b4-f08c-4dfd-b39e-6613a6d38b06
-        matchStudent(experiences, "9ec171cd-39dd-4785-9873-16d60439b7c2").then(result => {
-            console.log("Done with register part 1")
-        })
-
-        // //Checks if all fields have been filled in
-        // if ($w('#firstNameInput').valid && $w('#lastNameInput').valid && $w('#addressInput').valid && $w('#dateofbirthInput').valid && $w('#phoneInput').valid && $w('#nationalityInput').valid && $w('#emailInput').valid && $w('#passwordInput').valid) {
-        //     registerStudentAccount()
-        // }
-        // else {
-        //     console.log("Not all fields filled in");
-        // }
+        //Checks if all fields have been filled in
+        if ($w('#firstNameInput').valid && $w('#lastNameInput').valid && $w('#addressInput').valid && $w('#dateofbirthInput').valid && $w('#phoneInput').valid && $w('#nationalityInput').valid && $w('#emailInput').valid && $w('#passwordInput').valid) {
+            registerStudentAccount()
+        }
+        else {
+            console.log("Not all fields filled in");
+        }
     });
 
     function registerStudentAccount() {
@@ -69,73 +57,6 @@ $w.onReady(function () {
 
 });
 
-// SINGLE student matching to ALL vacancy.
-async function matchStudent(experiences, cv_id) {
-    const THRESHOLD = 0.6;
-    wixData.query("Vacancies")
-        .find()
-        .then(async (results) => {
-            if (results.items.length > 0) {
-                let arrayOfMatches = [];
-                for (let i = 0; i < results.length; i++) {
-
-                    const vacancy = results.items[i];
-                    const qualifications = vacancy.qualifications;
-
-                    let arrayOfQualifications = [];
-                    let arrayOfHowLong = [];
-
-
-                    for (let j = 0; j < qualifications.length; j++) {
-                        arrayOfQualifications.push(qualifications[j]["qualification"]);
-                        arrayOfHowLong.push(qualifications[j]["howLong"]);
-                    }
-
-                    // TODO: This code below block should be on the overview page for R'Cruit
-
-
-                    await getScore(arrayOfQualifications, experiences).then(score => {
-                        console.log(`The % match is: ${score * 100}%`);
-
-                        if (score > THRESHOLD) {
-                            arrayOfMatches.push(vacancy._id)
-                        }
-                    }).catch((err) => {
-                        console.log(err);
-                    })
-
-                    // TODO: This code block above should be on the overview page for R'Cruit
-
-                }
-
-                let matchingData = {
-                    "_id": cv_id,
-                    "cvReference": cv_id
-                };
-
-                wixData.save("MatchingData", matchingData)
-                    .then((results) => {
-                        wixData.replaceReferences("MatchingData", "vacancyReference", cv_id, arrayOfMatches)
-                            .then(() => {
-                                console.log("Vacancy matches updated!");
-                            })
-                            .catch((error) => {
-                                console.log(error);
-                            });
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    })
-
-
-            } else {
-                console.log("No vacancy match found.")
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
 
 
 
