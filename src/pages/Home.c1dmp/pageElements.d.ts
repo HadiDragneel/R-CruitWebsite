6af9254994119a63090687d27f6dd1768fcd7618
BSwamHR <0966726@hr.nl>
2020-06-09 16:01:38 +0200

    Removed wix account button

--- src/pages/Home.c1dmp/pageElements.d.ts
+++ src/pages/Home.c1dmp/pageElements.d.ts
@@ -39,7 +39,6 @@ type PageElementsMap = {
   "#header1": $w.Header;
   "#image2": $w.Image;
   "#horizontalMenu1": $w.Menu;
-  "#accountNavBar1": $w.AccountNavBar;
   "#container1": $w.HiddenCollapsedElement;
   "#verticalMenu1": $w.Menu;
   "#profile1": $w.IFrame;
