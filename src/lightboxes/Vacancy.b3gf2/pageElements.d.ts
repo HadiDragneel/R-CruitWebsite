55c20d6732353d2e905bfdf8d49fe112cb15fd2a
HadiDragneel <hadi.zebeleh@gmail.com>
2020-06-09 17:34:15 +0200

    UserId is now pushed to database under the _id field for all accounts.

--- src/lightboxes/Vacancy.b3gf2/pageElements.d.ts
+++ src/lightboxes/Vacancy.b3gf2/pageElements.d.ts
@@ -1,29 +1,2 @@
 type PageElementsMap = {
-  "#page1": $w.Page;
-  "#lightbox1": $w.HiddenCollapsedElement;
-  "#box1": $w.Box;
-  "#text78": $w.Text;
-  "#close": $w.VectorImage;
-  "#text73": $w.Text;
-  "#text74": $w.Text;
-  "#text75": $w.Text;
-  "#text76": $w.Text;
-  "#text77": $w.Text;
-  "#text79": $w.Text;
-  "#text81": $w.Text;
-  "#repeater1": $w.Repeater;
-  "#container": $w.Container;
-  "#QualificationItem": $w.Text;
-  "#QualificationTime": $w.Text;
-  "#line1": $w.HiddenCollapsedElement;
-  "#text72": $w.Text;
-  "#text82": $w.Text;
-  "#text83": $w.Text;
-  "#text84": $w.Text;
-  "#text86": $w.Text;
-  "#text87": $w.Text;
-  "#text88": $w.Text;
-  "#line3": $w.HiddenCollapsedElement;
-  "#wageHeader": $w.Text;
-  "#wageInfo": $w.Text;
 }
